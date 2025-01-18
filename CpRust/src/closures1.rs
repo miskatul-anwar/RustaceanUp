c5a392b8df9ef7fb53ed27ec32c13ce67704559f
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-01-18 10:36:40 +0600

    updated

--- CpRust/src/closures1.rs
+++ CpRust/src/closures1.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let mut name: Vec<char> = vec!['A', 'B', 'C'];
+    let mut name: Vec<char> = vec!['A', 'B', 'C', 'D'];
 
     name = name.iter().flat_map(|i| i.to_lowercase()).collect();
+    name.iter().map(|i| print!("{i:?} ")).for_each(drop);
+    println!();
 
     let mut numbers: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
 
     numbers = numbers.iter().map(|i| i + 1).collect();
-
-    println!("{:?}", name);
-    println!("{:?}", numbers);
+    numbers.iter().map(|i| print!("{i:?} ")).for_each(drop);
 }
