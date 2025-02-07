9165d271cd6a92abde2497beef1e4da46ea299da
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-07 13:38:26 +0600

    updated

--- sorting_problems/src/a_dragons.rs
+++ sorting_problems/src/a_dragons.rs
@@ -4,45 +4,55 @@
  *                                THANK YOU                                *
  ***************************************************************************/
 
-#![allow(unused)]
+use std::io::{self, BufRead};
 
-use std::io::{stdin, BufRead};
-
-fn rin_int() -> usize {
-    let mut input = String::new();
-    stdin().lock().read_line(&mut input).unwrap();
-    return input.trim().parse().unwrap();
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Pair {
+    x: usize,
+    y: usize,
 }
 
 fn rin_vec_int() -> Vec<usize> {
+    let stdin = io::stdin();
     let mut input = String::new();
-    stdin().lock().read_line(&mut input).unwrap();
-    let arr: Vec<usize> = input
+    stdin.lock().read_line(&mut input).unwrap();
+    input
         .trim()
         .split_whitespace()
         .map(|x| x.parse().unwrap())
-        .collect();
-    return arr;
-}
-
-fn rin_vec_char() -> Vec<char> {
-    let mut input = String::new();
-    stdin().lock().read_line(&mut input).unwrap();
-    let vec: Vec<char> = input.trim().chars().collect();
-    return vec;
-}
-
-fn to_string(vec: Vec<char>) -> String {
-    return vec.iter().collect::<String>();
-}
-
-fn solve() {
-    /* *************************(  Code Here  )************************* */
+        .collect()
 }
 
 pub fn main() {
-    let t = rin_int();
-    for _ in 0..t {
-        solve();
+    let line1 = rin_vec_int();
+    let (mut s, n) = (line1[0], line1[1]);
+
+    let mut xy: Vec<Pair> = Vec::new();
+
+    for _ in 1..=n {
+        let line = rin_vec_int();
+        xy.push(Pair {
+            x: line[0],
+            y: line[1],
+        });
+    }
+
+    xy.sort_unstable();
+
+    let mut possible = true;
+
+    for pair in xy {
+        if s > pair.x {
+            s += pair.y;
+        } else {
+            possible = false;
+            break;
+        }
+    }
+
+    if possible {
+        println!("YES");
+    } else {
+        println!("NO");
     }
 }
---
+++ sorting_problems/target/debug/a_dragons.d
@@ -0,0 +1,1 @@
+/mnt/d/CS/RustaceanUp/sorting_problems/target/debug/a_dragons: /mnt/d/CS/RustaceanUp/sorting_problems/src/a_dragons.rs
