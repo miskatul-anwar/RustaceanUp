df3516de2f3cf3982ee5801a22c0b7020b6c53cf
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-09 17:31:36 +0600

    updated

--- sorting_problems/src/b_basketball_together.rs
+++ sorting_problems/src/b_basketball_together.rs
@@ -22,9 +22,23 @@ fn rin_vec_int() -> Vec<usize> {
 }
 
 fn main() {
-    /* FIXME: */
     let (n_d, mut p) = (rin_vec_int(), rin_vec_int());
     let (n, d) = (n_d[0], n_d[1]);
 
     p.sort_unstable_by(|a, b| b.cmp(a));
+
+    let (mut player, mut i, mut wins) = (0, 0, 0);
+
+    while i < n && player <= n {
+        let x = ((d + 1) as f64 / (p[i] as f64 * 1.0)).ceil();
+        player += x as usize;
+
+        if player <= n {
+            wins += 1
+        }
+
+        i += 1
+    }
+
+    println!("{}", wins)
 }
