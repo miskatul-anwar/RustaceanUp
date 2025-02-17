1dc97f65c98db1c531c406ee3df5f6b80586a17f
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-17 21:30:31 +0600

    updated

--- prob/src/c_prepend_append.rs
+++ prob/src/c_prepend_append.rs
@@ -1,150 +1,40 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
- * THIS TEMPLATE IS MADE BY MISKATUL ANWAR<miskatul.anwar.csecu@gmail.com> *
- *               GITHUB : https://github.com/miskatul-anwar                *
- *                                THANK YOU                                *
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 #![allow(unused)]
+use std::io::{stdin, stdout, BufWriter, Write};
 
-use std::io::{stdin, BufRead};
-
-macro_rules! read_line {
-    () => {{
-        let mut input = String::new();
-        stdin().lock().read_line(&mut input).unwrap();
-        input
-    }};
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
 }
-
-macro_rules! rin {
-    ($t:ty) => {{
-        let mut input = String::new();
-        stdin().lock().read_line(&mut input).unwrap();
-        input.trim().parse::<$t>().unwrap()
-    }};
-}
-
-macro_rules! rin_vec {
-    ($t:ty) => {{
-        let mut input = String::new();
-        stdin().lock().read_line(&mut input).unwrap();
-        input
-            .trim()
-            .split_whitespace()
-            .map(|x| x.parse::<$t>().unwrap())
-            .collect::<Vec<$t>>()
-    }};
-}
-
-macro_rules! rin_vec_char {
-    () => {{
-        let mut input = String::new();
-        stdin().lock().read_line(&mut input).unwrap();
-        input.trim().chars().collect::<Vec<char>>()
-    }};
-}
-
-macro_rules! xor_swap {
-    ($x:expr, $y:expr) => {
-        if $x != $y {
-            $x ^= $y;
-            $y ^= $x;
-            $x ^= $y;
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
         }
-    };
-}
-
-macro_rules! prefix_sum {
-    ($vec:expr) => {
-        $vec.iter()
-            .scan(0, |sum, &x| {
-                *sum += x;
-                Some(*sum)
-            })
-            .collect::<Vec<i32>>()
-    };
-}
-
-macro_rules! max {
-    ($vec:expr) => {
-        *$vec.iter().max().unwrap()
-    };
-}
-
-macro_rules! min {
-    ($vec:expr) => {
-        *$vec.iter().min().unwrap()
-    };
-}
-
-macro_rules! sort_des {
-    ($vec:expr) => {
-        $vec.sort_unstable_by(|a, b| b.cmp(a));
-    };
-}
-
-macro_rules! vec_to_string {
-    ($vec:expr) => {
-        $vec.iter().collect::<String>()
-    };
-}
-
-macro_rules! string_to_vec {
-    ($string:expr) => {
-        $string.chars().collect::<Vec<char>>()
-    };
-}
-
-macro_rules! stot {
-    ($type:ty, $value:expr) => {
-        $value.parse::<$type>().expect("Conversion failed")
-    };
-}
-
-macro_rules! rout_vec {
-    ($vec:expr) => {{
-        for i in $vec {
-            print!("{} ", i)
-        }
-        print!("\n")
-    }};
-}
-
-macro_rules! is_even {
-    ($num:expr) => {
-        $num % 2 == 0
-    };
-}
-
-macro_rules! is_odd {
-    ($num:expr) => {
-        $num % 2 != 0
-    };
-}
-
-macro_rules! routln {
-    ($x:expr) => {
-        print!("{}\n", $x)
-    };
-}
-
-macro_rules! nl {
-    () => {
-        print!("\n");
-    };
-}
-
-/*~~~~~~~~~~~~~*
- * CODE BELOW: *
- *~~~~~~~~~~~~~*/
-fn solve() {
-    let n = rin!(usize);
-    let s = rin_vec_char!();
+    }
 }
 
 fn main() {
-    let t = rin!(usize);
+    let mut sc = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let t = sc.next();
     for _ in 1..=t {
-        solve()
+        let n = sc.next::<i32>();
+
+        let s: Vec<char> = sc.next::<String>().chars().into_iter().collect();
+
+        let (mut l, mut r) = (0, n - 1);
+
+        while l < r && s[l as usize] != s[r as usize] {
+            l += 1;
+            r -= 1;
+        }
+
+        writeln!(out, "{} ", r - l + 1);
     }
 }
