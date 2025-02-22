6d114f74a110381ceaeb96e31af80f0542a1dee9
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-02-22 22:52:29 +0600

    On branch main  Your branch is up to date with 'origin/main'.

--- graphviz/src/main.rs
+++ graphviz/src/main.rs
@@ -1,54 +1,60 @@
-use graphviz_rust::dot_generator::*;
-use graphviz_rust::dot_structures::*;
-use graphviz_rust::{attributes::*, cmd::Format, exec, parse, printer::PrinterContext};
+//! This is the best way to draw graphs in rust!!!
+
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::DiGraph;
 use std::fs::File;
 use std::io::Write;
+use std::process::Command;
 
-fn parse_test() {
-    let dot_data = r#"
-        strict digraph t {
-            aa[color=green]
-            subgraph v {
-                aa[shape=square]
-                subgraph vv{a2 -> b2}
-                aaa[color=red]
-                aaa -> bbb
-            }
-            aa -> be -> subgraph v { d -> aaa}
-            aa -> aaa -> v
-        }
-    "#;
+fn generate_graph() {
+    let mut graph = DiGraph::new();
 
-    match parse(dot_data) {
-        Ok(g) => {
-            println!("Parsed The Graph Successfully!: {:?}", g);
-        }
-        Err(e) => {
-            eprintln!("Error parsing DOT data: {:?}", e);
-        }
+    let aa = graph.add_node("aa");
+    let be = graph.add_node("be");
+    let d = graph.add_node("d");
+    let aaa = graph.add_node("aaa");
+    let bbb = graph.add_node("bbb");
+    let a2 = graph.add_node("a2");
+    let b2 = graph.add_node("b2");
+
+    graph.add_edge(aa, be, "");
+    graph.add_edge(be, d, "");
+    graph.add_edge(d, aaa, "");
+    graph.add_edge(aa, aaa, "");
+    graph.add_edge(aaa, bbb, "");
+    graph.add_edge(a2, b2, "");
+
+    let dot_format = format!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+
+    let mut file = File::create("bin/output_graph.dot").expect("Failed to create file");
+    file.write_all(dot_format.as_bytes())
+        .expect("Failed to write to file");
+    println!("Graph saved as output_graph.dot");
+
+    generate_svg()
+}
+
+fn generate_svg() {
+    let output = Command::new("dot")
+        .args([
+            "-Tsvg",
+            "bin/output_graph.dot",
+            "-o",
+            "assets/output_graph.svg",
+        ])
+        .output()
+        .expect("Failed to execute Graphviz");
+
+    if output.status.success() {
+        println!("SVG generated: output_graph.svg");
+    } else {
+        eprintln!(
+            "Error generating SVG: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
     }
 }
 
-fn output_test() {
-    let g = graph!(id!("id");
-         node!("nod"),
-         subgraph!("sb";
-             edge!(node_id!("a") => subgraph!(;
-                node!("n";
-                NodeAttributes::color(color_name::black), NodeAttributes::shape(shape::egg))
-            ))
-        ),
-        edge!(node_id!("a1") => node_id!(esc "a2"))
-    );
-
-    let graph_svg = exec(g, &mut PrinterContext::default(), vec![Format::Svg.into()]).unwrap();
-
-    let mut file = File::create("output_graph.svg").expect("Failed to create file");
-    file.write_all(&graph_svg).expect("Failed to write to file");
-    println!("Graph saved as output_graph.svg");
-}
-
 fn main() {
-    parse_test();
-    output_test();
+    generate_graph();
 }
