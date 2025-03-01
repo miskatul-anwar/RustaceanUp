2e1728c4cee96bb60dd8167bdca2c90e42c5133d
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2025-03-01 09:10:27 +0600

    On branch main  Your branch is up to date with 'origin/main'.

---
+++ dsa/src/dfs-tree.rs
@@ -0,0 +1,41 @@
+#![allow(unused)]
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+/*~~~~~~~~~~~~~*
+ * CODE BELOW: *
+ *~~~~~~~~~~~~~*/
+fn dfs(node: usize, parent: usize, graph: &Vec<Vec<usize>>) {
+    for &child in &graph[node] {
+        if child == parent {
+            continue;
+        }
+
+        dfs(child, node, graph);
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let t = sc.next();
+    for _ in 1..=t {}
+}
